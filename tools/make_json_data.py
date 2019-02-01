cda195663a5d98c8ed3c54a4a05dd34445c783a0
henry199101 <331243543@qq.com>
2019-02-01 21:19:18 +0800

    增加 1 个工具函数，可以将【字典】转换为【JSON 格式的字符串】

---
+++ tools/make_json_data.py
@@ -0,0 +1,16 @@
+def make_readable_json_data_or_not(a_dict, readable):
+    import json
+
+    indent = 8 if readable else 0
+
+    return json.dumps(a_dict, sort_keys=True, indent=indent, separators=(',', ':'), ensure_ascii=False)
+    '''
+    >>> d
+    {'a': 1}
+    >>> import json
+    >>> r = json.dumps(d)
+    >>> r
+    '{"a": 1}'
+    >>> type(r)
+    <class 'str'>
+    '''
