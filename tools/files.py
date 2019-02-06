f8635b9c73bbdf16d34ce4aea9d2b3029ee19ce3
henry199101 <331243543@qq.com>
2019-02-06 22:35:34 +0800

    这个文件的意义在于，更方便地使用基础数据的txt。

---
+++ tools/files.py
@@ -0,0 +1,20 @@
+prefix = 'files/'
+
+# txt & json files
+total_txt           = prefix + 'baiketriples.txt'
+
+total_txt_1_to_10   = prefix + 'baiketriples_1_to_10_read_only.txt'
+
+total_txt_1_to_1000 = prefix + 'baiketriples_1_to_1000_read_only.txt'
+total_txt_1_to_1000_mapper_of_property_names_to_pinyins = \
+    prefix + 'baiketriples_1_to_1000_mapper_of_property_names_to_pinyins_read_only.txt'
+
+total_txt_1_to_10000 = prefix + 'baiketriples_1_to_10000.txt'
+
+total_txt_9_lines   = prefix + 'baiketriples_9_lines.txt'
+total_txt_9_lines_mapper_of_property_names_to_pinyins = \
+    prefix + 'baiketriples_9_lines_mapper_of_property_names_to_pinyins.txt'
+
+total_txt_49_lines  = prefix + 'baiketriples_49_lines.txt'
+total_txt_49_lines_mapper_of_property_names_to_pinyins = \
+    prefix + 'baiketriples_49_lines_mapper_of_property_names_to_pinyins.txt'
