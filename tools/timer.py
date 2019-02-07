f00d59629426c5d7044d4c2fae6c812c5467d7f4
henry199101 <331243543@qq.com>
2019-02-07 15:31:33 +0800

    新增 1 个工具函数，用于将秒数转化为 小时、分钟、秒数。

--- tools/timer.py
+++ tools/timer.py
@@ -4,6 +4,13 @@
 from time import time, sleep
 
 
+def get_hours_minutes_seconds(seconds):
+	hours	= seconds // 3600
+	minutes	= seconds // 60 % 60
+	seconds	= seconds % 60
+	return hours, minutes, seconds
+
+
 def get_time_or_not_for_method(get=True):
     def timer(method):
         def wrapper(self, *arg, **kwargs):
@@ -14,11 +21,16 @@ def get_time_or_not_for_method(get=True):
 
                 total_time = end_time - start_time
 
-                if total_time <= 60:
-                    print('耗时:%.2f 秒' % total_time)
-                else:
+                print('耗时：', end='')
+                if total_time < 60:
+                    print('%.2f 秒' % total_time)
+                elif 60 <= total_time <= 3559:
                     total_time /= 60
-                    print('耗时:%.2f 分钟' % total_time)
+                    print('%.2f 分钟' % total_time)
+                elif 3600 <= total_time:
+                    total_time /= 3600
+                    print('%.2f 小时' % total_time)
+                print('\n')
             else:
                 method(self, *arg, **kwargs)
 
@@ -66,3 +78,20 @@ if __name__ == '__main__':
     c.mysleep(0.01)
 
     a_sleep(0.02)
+
+    f = get_hours_minutes_seconds
+    assert f(1) == (0, 0, 1)
+    assert f(17) == (0, 0, 17)
+    assert f(59) == (0, 0, 59)
+    assert f(60) == (0, 1, 0)
+    assert f(61) == (0, 1, 1)
+    assert f(67) == (0, 1, 7)
+    assert f(119) == (0, 1, 59)
+    assert f(120) == (0, 2, 0)
+    assert f(121) == (0, 2, 1)
+    assert f(132) == (0, 2, 12)
+    assert f(3599) == (0, 59, 59)
+    assert f(3600) == (1, 0, 0)
+    assert f(3601) == (1, 0, 1)
+    assert f(3763) == (1, 2, 43)
+    print('Pass!')
