83d28606d6822163926a6f8c17af7290b0cf1473
henry199101 <331243543@qq.com>
2019-02-09 14:49:28 +0800

    为程序设置密钥，不然表单的 GET、POST 会报错。原因见【flask 李辉 P106】。

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+from forms import SearchForm
 
 
 app = Flask(__name__)
+app.secret_key = 'secret key'
 
 
 @app.route('/')
 @app.route('/index')
 def hello():
     return 'Hello World!'
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    form = SearchForm()
+
+    if form.validate() and request.method == 'post':
+        return 'post page'
+    return 'get page'
