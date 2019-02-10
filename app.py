55b25ebb25538564e4dd85b9c6d3752244db7ab7
henry199101 <331243543@qq.com>
2019-02-10 11:39:39 +0800

    该视图函数有3个功能：【渲染原始的search表单】【精确匹配且没有对应的查询结果】【精确匹配且有对应的查询结果】

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template, redirect, url_for
-from forms import SearchForm
+from flask import Flask, request, render_template, redirect, url_for, flash
+from .forms import SearchForm
+from .import_data_to_graph_db import DomainModel
 
 
 app = Flask(__name__)
@@ -17,5 +18,17 @@ def search():
     form = SearchForm()
 
     if form.validate_on_submit():
-        return 'post page'
-    return render_template('search.html', form=form)
+        entity_name = form.search_content.data
+        form.search_content.data = None
+
+        domain_model = DomainModel()
+        query_result = domain_model.query(entity_name=entity_name)
+
+        if query_result is None:
+            return render_template('search.html', form=form, entity_name=entity_name, query_result=None)
+
+        labels = query_result.labels
+        keys = query_result.keys()
+        return render_template('search.html', form=form, entity_name=entity_name, query_result=query_result, labels=labels, keys=keys)
+
+    return render_template('search.html', form=form, query_string=None)
